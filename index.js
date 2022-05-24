07973948d7c4a1a3944a2984d2fcbcdea68dbeea
John Doe <johndoe@email.com>
2022-05-24 22:15:35 +0200

    fix

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 
+app.use("/", express.static("./www"))
+
+
 router.get("/", async function(req, res){
     res.send("i'm alive")
 });
